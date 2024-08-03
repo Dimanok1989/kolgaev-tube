93c7bdceaa02b88968d12c6d1537a744142e824a
Dimanok1989 <kolgaev@gmail.com>
2024-08-03 12:10:13 +0300

    download

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Jobs\StartDownloadJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DownloadsController extends Controller
 {
@@ -22,5 +23,25 @@ class DownloadsController extends Controller
         return response()->json([
             'message' => "OK"
         ]);
+    }
+
+    /**
+     * Скачивание файла
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param string $folder
+     * @param string $file
+     * @return 
+     */
+    public function download(Request $request, string $folder, string $file)
+    {
+        $path = collect(['youtube', $folder, urldecode($file)])->join(DIRECTORY_SEPARATOR);
+
+        $storage = Storage::disk('local');
+        abort_if(!$storage->exists($path), 404);
+
+        return response()->file(
+            $storage->path($path)
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\DownloadsController;
+use App\Http\Middleware\ProcessTokenMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    abort(404);
     return view('welcome');
 });
+
+Route::get('youtube/{folder}/{file}', [DownloadsController::class, 'download'])
+    ->middleware(ProcessTokenMiddleware::class);
